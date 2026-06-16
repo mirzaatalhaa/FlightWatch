834d13d4946ef30734c793dfbf43c62ccd29150b
mirzaatalhaa <144937151+mirzaatalhaa@users.noreply.github.com>
2026-06-16 15:24:30 +0530

    Updated the cloudfront.tf with certifications

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -2,6 +2,49 @@ resource "aws_cloudfront_distribution" "flightwatch_frontend" {
   enabled             = true
   default_root_object = "index.html"
 
+  aliases = ["flight-watch.xyz"]
+
+  origin {
+    domain_name = aws_s3_bucket.flightwatch_frontend.website_endpoint
+    origin_id   = "flightwatch-s3-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+
+      origin_ssl_protocols = [
+        "TLSv1.2"
+      ]
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id = "flightwatch-s3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    compress = true
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -9,6 +52,8 @@ resource "aws_cloudfront_distribution" "flightwatch_frontend" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn      = "arn:aws:acm:us-east-1:424322298959:certificate/c8866cbd-9873-4ba7-8d2b-4d0c3d93116a"
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
