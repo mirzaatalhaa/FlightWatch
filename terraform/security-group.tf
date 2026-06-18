8e5c0b26c216f2de78746a773c901c19bb94ce73
mirzaatalhaa <144937151+mirzaatalhaa@users.noreply.github.com>
2026-06-18 16:29:53 +0530

    Edit security inbound rules

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -17,12 +17,7 @@ resource "aws_security_group" "flightwatch_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
 
   ingress {
     from_port   = 443
