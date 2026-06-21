f320c57a2bfefe51bb0f86579618420a8e361214
mirzaatalhaa <144937151+mirzaatalhaa@users.noreply.github.com>
2026-06-22 00:24:53 +0530

    Attach IAM role

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "flightwatch_backend" {
   instance_type = "t3.micro"
 
   subnet_id = aws_subnet.flightwatch_public_subnet.id
-
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids = [
     aws_security_group.flightwatch_security_group.id
   ]
